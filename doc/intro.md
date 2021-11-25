3277134989bec93c06708f5340126d74a8ec8eb2
Salvador Guzman <guzmansalv@gmail.com>
2021-11-24 19:26:13 -0800

    ok this is now a tesseract repo :) :O

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clean-up-pdf
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/clean_up_pdf/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clean-up-pdf.core-test
-  (:require [clojure.test :refer :all]
-            [clean-up-pdf.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
